3593698a25b4044eee6030abbda9bf31b5796ebc
Doston777 <dmamadaminov7@gmail.com>
2021-12-22 17:38:48 +0500

    fixed bugs: added id header

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container header">
+  <div class="container header" id="header">
     <p class="welcome">Welcome!</p>
     <h1>My name is John Doe</h1>
     <h2>I'm a passionate <span>web blogger</span> from CA</h2>
