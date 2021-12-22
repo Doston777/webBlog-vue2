ec2cbd1328b856721bf025981718bb086a42df68
Doston777 <dmamadaminov7@gmail.com>
2021-12-22 17:28:50 +0500

    edited paragraph of articles

--- src/components/Articles.vue
+++ src/components/Articles.vue
@@ -2,8 +2,8 @@
   <section class="row articles" id="articles">
     <h2>Articles</h2>
     <div class="col-md-6 article" v-for="n in 8" v-bind:key="n">
-      <h2>Lorem ipsum dolor sit amet.</h2>
       <img :src="`img/articles/article-${n}.jpg`" alt="">
+      <h2>Lorem ipsum dolor sit amet.</h2>
       <p>Nullam sit amet accumsan lacus. Integer tincidunt, diam at tempus porttitor, lacus diam euismod purus, aliquet
         accumsan massa purus non lectus. Mauris fermentum libero mauris, ac semper ex pellentesque molestie. Quisque ac
         diam fermentum, consequat enim sed, elementum magna. Vestibulum ut tellus id quam malesuada interdum at ut urna.
